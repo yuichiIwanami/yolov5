7d64ce48cece0ab889cfc498edbe2ca78774c72b
iwanami yuichi <gaudi8@gmail.com>
2022-02-04 00:39:59 +0900

    fix log msg

--- utils/loggers/__init__.py
+++ utils/loggers/__init__.py
@@ -44,9 +44,13 @@ class Loggers():
         self.logger = logger  # for printing results to console
         self.include = include
         self.keys = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
-                     'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
+                     'metrics/precision', 'metrics/recall', 'metrics/f2', 'metrics/mAP_0.3', 'metrics/mAP_0.3:0.8',  # metrics
                      'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
                      'x/lr0', 'x/lr1', 'x/lr2']  # params
+        # self.keys = ['train/box_loss', 'train/obj_loss', 'train/cls_loss',  # train loss
+        #              'metrics/precision', 'metrics/recall', 'metrics/mAP_0.5', 'metrics/mAP_0.5:0.95',  # metrics
+        #              'val/box_loss', 'val/obj_loss', 'val/cls_loss',  # val loss
+        #              'x/lr0', 'x/lr1', 'x/lr2']  # params
         self.best_keys = ['best/epoch', 'best/precision', 'best/recall', 'best/mAP_0.5', 'best/mAP_0.5:0.95',]
         for k in LOGGERS:
             setattr(self, k, None)  # init empty logger dictionary
